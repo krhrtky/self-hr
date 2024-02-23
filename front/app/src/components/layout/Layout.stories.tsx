1a2f1955dfb68c8c75569c58dad06913fab5ecb8
takuya.kurihara <takuya.kurihara00@gmail.com>
2024-02-23 16:10:37 +0900

    note: tanstack router コンポーネントをレンダリングできないのでコメントアウトとテストを削除。

--- front/app/src/components/layout/Layout.stories.tsx
+++ front/app/src/components/layout/Layout.stories.tsx
@@ -1,4 +1,7 @@
-import { Meta, StoryObj } from "@storybook/react";
+import {
+  Meta,
+  //StoryObj,
+} from "@storybook/react";
 import { Layout } from "./Layout";
 
 const meta: Meta<typeof Layout> = {
@@ -7,13 +10,14 @@ const meta: Meta<typeof Layout> = {
 
 export default meta;
 
-type Story = StoryObj<typeof Layout>;
+//type Story = StoryObj<typeof Layout>;
 
-const Child = () => (
-  <div className="min-h-screen overflow-y-auto">main content</div>
-);
-export const Basic: Story = {
-  args: {
-    children: <Child />,
-  },
-};
+//const Child = () => (
+//   <div className="min-h-screen overflow-y-auto">main content</div>
+// );
+// @see https://github.com/TanStack/router/discussions/952
+// export const Basic: Story = {
+//   args: {
+//     children: <Child />,
+//   },
+// };
