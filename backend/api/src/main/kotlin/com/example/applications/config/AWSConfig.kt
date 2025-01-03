db7024891093b42e3c59970b4283a49f31ea0e33
takuya.kurihara <takuya.kurihara00@gmail.com>
2025-01-03 17:39:36 +0900

    add: aws config.

--- backend/api/src/main/kotlin/com/example/applications/config/AWSConfig.kt
+++ backend/api/src/main/kotlin/com/example/applications/config/AWSConfig.kt
@@ -8,4 +8,6 @@ import org.springframework.stereotype.Component
 class AWSConfig {
     lateinit var overrideUrl: String
     lateinit var region: String
+    lateinit var userPoolId: String
+    lateinit var clientPoolId: String
 }
